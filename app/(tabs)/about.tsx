f1dfbb6a3af5b7e55eb37b7ef98aed32603af934
Thaís Abe <159479714+thaisqabe@users.noreply.github.com>
2025-09-04 12:08:19 -0300

    Update about.tsx

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>About screen</Text>
+      <Text style={styles.text}>Ana Clara Rivas e Thaís Abe</Text>
     </View>
   );
 }
@@ -19,3 +19,4 @@ const styles = StyleSheet.create({
     color: '#fff',
   },
 });
+
